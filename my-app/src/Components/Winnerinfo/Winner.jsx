b2907fc0363880bde61f5a995f7a4225eeb751ef
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-10-28 11:26:14 +0300

    N13 fix empty array

--- my-app/src/Components/Winnerinfo/Winner.jsx
+++ my-app/src/Components/Winnerinfo/Winner.jsx
@@ -8,26 +8,38 @@ import {getWinner, backTo} from '../../Actions/userActions';
 export const Winner = () => {
     const {users, winner, isWinner} = useSelector(state => state.users);
     const dispatch = useDispatch();
+    let empty = false; 
+    if (!users.length) {
+        empty = true;
+    };
     return ( 
         <MainContainer sx={{mt: 5}}>
             {
                 isWinner
                 ?
                 <div>
-                    <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>The Winner</Typography>
-                    <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>ID: {winner.id}</Typography>
-                    <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>Name: {winner.name}</Typography>
-                    <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>Surname: {winner.second}</Typography>
-                    <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>Time: {winner.time}</Typography>
-                    <PrimaryButton onClick = {()=> dispatch(backTo())}>Back to initial state</PrimaryButton>
+                    {
+                        empty
+                        ?
+                        <h1>Users array is empty</h1>
+                        :
+                         <div>
+                                <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>The Winner</Typography>
+                        <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>ID: {winner.id}</Typography>
+                        <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>Name: {winner.name}</Typography>
+                        <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>Surname: {winner.second}</Typography>
+                        <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>Time: {winner.time}</Typography>
+                        <PrimaryButton onClick = {()=> dispatch(backTo())}>Back to initial state</PrimaryButton>
+                         </div>
+                    }
                 </div>
                 :
                 <div>
+                    
                     <Typography component="h2" variant="h5" sx={{textAlign: 'center',mb:5}}>Total participants {users.length}</Typography>
                     <PrimaryButton onClick = {() => dispatch(getWinner())}>Show winners</PrimaryButton>
                 </div>
             }
-            
         </MainContainer>
      );
 }
